bbb69c0fb4b35c5a6fde1e0be0c3e88e89bd3677
adcavalcant <aderc19@gmail.com>
2023-04-09 22:07:30 -0400

    Tabela Produtos

--- frontend/src/app/components/product/product-read2/product-read2-datasource.ts
+++ frontend/src/app/components/product/product-read2/product-read2-datasource.ts
@@ -3,20 +3,15 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
-
-// TODO: Replace this with your own data model type
-export interface ProductRead2Item {
-  name: string;
-  id: number;
-}
+import { Product } from '../product.model';
 
 // TODO: replace this with real data from your application
-const EXAMPLE_DATA: ProductRead2Item[] = [
-  { id: 1, name: 'Hydrogen' },
-  { id: 2, name: 'Helium' },
-  { id: 3, name: 'Lithium' },
-  { id: 4, name: 'Beryllium' },
-  { id: 5, name: 'Boron' },
+const EXAMPLE_DATA: Product[] = [
+  { id: 1, name: 'Hydrogen', price: 19.5 },
+  { id: 2, name: 'Helium', price: 8.5 },
+  { id: 3, name: 'Lithium', price: 10.5 },
+  { id: 4, name: 'Beryllium', price: 5.5 },
+  { id: 5, name: 'Boron', price: 9.5 },
 ];
 
 /**
@@ -24,8 +19,8 @@ const EXAMPLE_DATA: ProductRead2Item[] = [
  * encapsulate all logic for fetching and manipulating the displayed data
  * (including sorting, pagination, and filtering).
  */
-export class ProductRead2DataSource extends DataSource<ProductRead2Item> {
-  data: ProductRead2Item[] = EXAMPLE_DATA;
+export class ProductRead2DataSource extends DataSource<Product> {
+  data: Product[] = EXAMPLE_DATA;
   paginator: MatPaginator | undefined;
   sort: MatSort | undefined;
 
@@ -38,7 +33,7 @@ export class ProductRead2DataSource extends DataSource<ProductRead2Item> {
    * the returned stream emits new items.
    * @returns A stream of the items to be rendered.
    */
-  connect(): Observable<ProductRead2Item[]> {
+  connect(): Observable<Product[]> {
     if (this.paginator && this.sort) {
       // Combine everything that affects the rendered data into one update
       // stream for the data-table to consume.
@@ -68,7 +63,7 @@ export class ProductRead2DataSource extends DataSource<ProductRead2Item> {
    * Paginate the data (client-side). If you're using server-side pagination,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getPagedData(data: ProductRead2Item[]): ProductRead2Item[] {
+  private getPagedData(data: Product[]): Product[] {
     if (this.paginator) {
       const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
       return data.splice(startIndex, this.paginator.pageSize);
@@ -81,7 +76,7 @@ export class ProductRead2DataSource extends DataSource<ProductRead2Item> {
    * Sort the data (client-side). If you're using server-side sorting,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getSortedData(data: ProductRead2Item[]): ProductRead2Item[] {
+  private getSortedData(data: Product[]): Product[] {
     if (!this.sort || !this.sort.active || this.sort.direction === '') {
       return data;
     }
@@ -92,7 +87,7 @@ export class ProductRead2DataSource extends DataSource<ProductRead2Item> {
         case 'name':
           return compare(a.name, b.name, isAsc);
         case 'id':
-          return compare(+a.id, +b.id, isAsc);
+          return compare(+a.id!, +b.id!, isAsc);
         default:
           return 0;
       }

5515d91e902e4a01dee0cca618c5ca3d9175a494
adcavalcant <aderc19@gmail.com>
2023-04-30 04:07:47 -0300

    Ajustes Token JWT

--- api-laravel/routes/api.php
+++ api-laravel/routes/api.php
@@ -5,4 +5,16 @@ Route::apiResource('clientes', 'App\Http\Controllers\Api\ClienteController');
 Route::apiResource('products', 'App\Http\Controllers\Api\ProductController');
 Route::apiResource('pensamentos', 'App\Http\Controllers\Api\PensamentoController');
 
-Route::get('users', 'App\Http\Controllers\Api\\UserController@index');
+Route::get('users', 'App\Http\Controllers\Api\UserController@index');
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+
+    Route::post('login', 'App\Http\Controllers\Api\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\Api\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\Api\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\Api\AuthController@me');
+
+});
